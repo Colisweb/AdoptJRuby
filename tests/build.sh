6250c152c9d313e8118f57a34136d2c03a6146f0
jules Ivanic <jules.ivanic@gmail.com>
2019-03-10 19:19:32 +0100

    Add corretto in the comparaison list

--- tests/build.sh
+++ tests/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 cp -r script adoptopenjdk
 cp -r script openjdk
+cp -r script corretto
 
+docker build corretto/. -t main-corretto
 docker build adoptopenjdk/. -t main-adoptopenjdk
 docker build openjdk/. -t main-openjdk
 
 rm -rf adoptopenjdk/script
 rm -rf openjdk/script
+rm -rf corretto/script
--- tests/generate-flags-files.sh
+++ tests/generate-flags-files.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 ./build.sh
 
 docker run main-adoptopenjdk > adoptopenjdk.txt
 
 docker run main-openjdk > openjdk.txt
+
+docker run main-corretto > corretto.txt
